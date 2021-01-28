89b0c184ff4903b0ebaceb70610768780d28604e
Luis Conde Bento <luis.conde@ipleiria.pt>
2021-01-28 12:25:20 +0000

    Update example.ino

--- Code_Example/example.ino
+++ Code_Example/example.ino
@@ -15,8 +15,6 @@
 //
 //-DESCRIPTION--------------------------------------------------------------
 //
-// Simulator initialization
-//
 //-DISCLAIMER---------------------------------------------------------------
 // This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
